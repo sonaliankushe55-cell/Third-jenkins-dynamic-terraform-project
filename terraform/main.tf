8f69951d92852812526678ec50c085afc3a4124b
Ubuntu <ubuntu@ip-172-31-14-10.us-west-1.compute.internal>
2026-06-18 12:46:04 +0000

    Add terraform configuration files

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "us-west-1c"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
